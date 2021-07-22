f6456fa456fdb929da4c18ec34097b4d8f8671c8
TSPraneeth <tpraneeth98@gmail.com>
2021-07-22 19:34:31 +0530

    submit

---
+++ script.js
@@ -0,0 +1,30 @@
+function ValidateEmailAddress(emailString) {
+  // check for @ sign
+  var atSymbol = emailString.indexOf("@");
+  if (atSymbol < 1) return false;
+
+  if(emailString === "") return false;
+
+  var dot = emailString.indexOf(".");
+  if (dot <= atSymbol + 2) return false;
+
+  // check that the dot is not at the end
+  if (dot === emailString.length - 1) return false;
+
+  return true;
+}
+
+function validate(emailString) {
+  //get result as true/false
+  var Result = ValidateEmailAddress(emailString);
+
+  if (!Result) {
+    document.getElementById("error").innerHTML = "Please provide a valid email";
+    document.getElementById("emaill").classList.add("invalid");
+  }
+  else
+  {
+    document.getElementById("error").innerHTML = "";
+    document.getElementById("emaill").classList.remove("invalid");
+  }
+}
